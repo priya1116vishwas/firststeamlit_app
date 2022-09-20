87688c629d0b7bb26fbf87cbe151a3bd17ee029f
Priya Vishwas <113985599+priya1116vishwas@users.noreply.github.com>
2022-09-20 17:28:46 +0530

    Update streamlit_app.py
    added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parent New Healthy Dinner')
-streamlit.header('Menu Brekfast')
-streamlit.text('Poha')
-streamlit.text('idli')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
